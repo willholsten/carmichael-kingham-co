95d94ecf718fa5961d0ed58bc58d482436d1d2ed
Will Holsten <willholsten@wills-mbp.home>
2019-11-05 16:16:21 +0000

    nav active styling

--- components/NavList/index.jsx
+++ components/NavList/index.jsx
@@ -1,5 +1,8 @@
+// Components
+import Link from "../../utils/ActiveLink";
+
 // Resources
-import Link from "next/link";
+// import Link from "next/link";
 
 // Styles
 import "../../styles/main.scss";
@@ -8,31 +11,31 @@ import styles from "./styles.scss";
 export default function NavList() {
   return (
     <ul className={styles.NavList}>
-      <Link href="/about-us">
+      <Link activeClassName="active" href="/about-us">
         <li>
           <img className={styles.NavMobile_icon} src="/svg/about-us.svg" />
           <a>About Us</a>
         </li>
       </Link>
-      <Link href="/our-people">
+      <Link activeClassName="active" href="/our-people">
         <li>
           <img className={styles.NavMobile_icon} src="/svg/our-people.svg" />
           <a>Our People</a>
         </li>
       </Link>
-      <Link href="/services">
+      <Link activeClassName="active" href="/services">
         <li>
           <img className={styles.NavMobile_icon} src="/svg/services.svg" />
           <a>Services</a>
         </li>
       </Link>
-      <Link href="/blog">
+      <Link activeClassName="active" href="/blog">
         <li>
           <img className={styles.NavMobile_icon} src="/svg/blog.svg" />
           <a>Blog</a>
         </li>
       </Link>
-      <Link href="/contact">
+      <Link activeClassName="active" href="/contact">
         <li>
           <img className={styles.NavMobile_icon} src="/svg/contact.svg" />
           <a>Contact</a>
