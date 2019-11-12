1a1ea38c80c4832ef32e7e076db9cad81cb0b9bb
Will Holsten <willholsten@Wills-MacBook-Pro.local>
2019-11-12 10:58:22 +0000

    add primaryButton, and env file

---
+++ components/Common/PrimaryButton/index.jsx
@@ -0,0 +1,18 @@
+// Resources
+import Link from "next/link";
+
+// Styles
+import styles from "./styles.scss";
+
+export default function PrimaryButton({ href, text, icon }) {
+  return (
+    <div className={styles.Button}>
+      <Link href={href}>
+        <a>
+          {icon && <i className={`${icon} ${styles.buttonIcon}`}></i>}
+          {text}
+        </a>
+      </Link>
+    </div>
+  );
+}
