164b5837ec7be9684f651dd66f3c30b9d865ff2a
Will Holsten <willholsten@wills-mbp.home>
2020-03-20 16:45:48 +0000

    footer partners styling

--- components/FooterPartners/index.jsx
+++ components/FooterPartners/index.jsx
@@ -1,12 +1,12 @@
 // Resources
-import Link from "next/link";
+import Link from 'next/link';
 
 // Styles
 
-import styles from "./styles.scss";
+import styles from './styles.scss';
 
 // Data
-import content from "../../content/navigation.md";
+import content from '../../content/navigation.md';
 
 export default class FooterPartners extends React.Component {
   render() {
@@ -17,11 +17,7 @@ export default class FooterPartners extends React.Component {
       <div className={styles.FooterPartners}>
         <h4>Partners</h4>
         {partners.map(partner => (
-          <Link key={partner.icon} href={partner.href}>
-            <a>
-              <img src={partner.icon} alt={partner.label} />
-            </a>
-          </Link>
+          <img key={partner.label} src={partner.icon} alt={partner.label} />
         ))}
       </div>
     );
